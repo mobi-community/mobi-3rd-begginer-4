a2c00de81025fc28cad6d54dce40da697f669c60
jjjiyoung0130 <jiyoung63@nate.com>
2024-03-27 17:00:44 +0900

    chore: 과제5 react-error-boundary 의존성 추가

---
+++ week4-5/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import ErrorComponent from "./component/ErrorComponent";
+import { ErrorBoundary } from "react-error-boundary";
+
+function ErrorFallback(err) {
+  if (err.error.status === 401) {
+    console.log(err.error.message);
+  }
+  return <div>예기치 못한 에러가 발생하였습니다</div>;
+}
+
+function App() {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <ErrorComponent />
+    </ErrorBoundary>
+  );
+}
+
+export default App;
---
+++ week4-5/src/component/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+const ErrorComponent = () => {
+  const err = new Error();
+  err.message = "인증되지 않은 회원입니다";
+  err.status = 401;
+  throw err;
+};
+export default ErrorComponent;
