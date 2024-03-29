2047829236cf342a1b5e881a1c684953978ac513
jinsol <98089768+ijimlnosk@users.noreply.github.com>
2024-03-29 23:35:36 +0900

    로직 및 주석 수정

--- mobi-error/src/App.jsx
+++ mobi-error/src/App.jsx
@@ -3,17 +3,10 @@ import ErrorComponent from "./component/ErrorComponent";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallBack from "./component/ErrorFallback";
 
-// function ErrorFallback(err) {
-//   if (err.error.status === 401) {
-//     console.log(err.error.message);
-//   }
-//   return <div>예기치 못한 에러가 발생하였습니다</div>;
-// }
-
 function App() {
     return (
         <ErrorBoundary FallbackComponent={ErrorFallBack}>
-            <ErrorComponent error={401} />
+            <ErrorComponent error={404} />
         </ErrorBoundary>
     );
 }
--- mobi-error/src/component/ErrorFallback.jsx
+++ mobi-error/src/component/ErrorFallback.jsx
@@ -1,18 +1,20 @@
-const ErrorFallBack = ({ error }) => {
+const ErrorFallBack = ({ error, resetErrorBoundary }) => {
     let errorMsg = "예기치 못한 에러가 발생했습니다.";
 
-    if (error.status === 401) {
-        errorMsg = "인증되지 않은 회원입니다.";
-    } else if (error.status === 404) {
-        errorMsg = "요청한 페이지를 찾을 수 없습니다.";
-    } else if (error.status === 500) {
-        errorMsg = "서버 에러 발생.";
+    switch (error.status) {
+        case 401:
+            errorMsg = "인증되지 않은 사용자입니다.";
+        case 404:
+            errorMsg = "요청한 페이지를 찾을 수 없습니다.";
+        case 500:
+            errorMsg = "서버에서 에러가 발생했습니다.";
     }
 
     return (
         <div>
             <p>{errorMsg}</p>
             <p> 상세 : {error.message}</p>
+            <button onClick={() => resetErrorBoundary()}>재시도</button>
         </div>
     );
 };
