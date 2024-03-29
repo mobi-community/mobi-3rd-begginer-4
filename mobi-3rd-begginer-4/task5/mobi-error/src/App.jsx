925fe5b163c32043ba028a4123199c12d3695200
hayoung78 <wendy4332@naver.com>
2024-03-29 22:45:20 +0900

    feat: 에러바운더리 사용

--- mobi-3rd-begginer-4/task5/mobi-error/src/App.jsx
+++ mobi-3rd-begginer-4/task5/mobi-error/src/App.jsx
@@ -3,18 +3,21 @@ import ErrorComponent from "./component/ErrorComponent";
 import { ErrorBoundary } from "react-error-boundary";
 
 function ErrorFallback(err) {
-  if (err.error.status === 401) {
-    console.log(err.error.message);
-  }
-  return <div>예기치 못한 에러가 발생하였습니다</div>;
+    if (err.error.status === 401) {
+        console.log(err.error.message);
+    }
+    if (err.error.status === 404) {
+        return <div>페이지를 찾을 수 없습니다.</div>;
+    }
+    return <div>예기치 못한 에러가 발생하였습니다</div>;
 }
 
 function App() {
-  return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <ErrorComponent />
-    </ErrorBoundary>
-  );
+    return (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <ErrorComponent error={404} />
+        </ErrorBoundary>
+    );
 }
 
 export default App;
--- mobi-3rd-begginer-4/task5/mobi-error/src/component/ErrorComponent.jsx
+++ mobi-3rd-begginer-4/task5/mobi-error/src/component/ErrorComponent.jsx
@@ -1,7 +1,28 @@
-const ErrorComponent = () => {
-  const err = new Error();
-  err.message = "인증되지 않은 회원입니다";
-  err.status = 401;
-  throw err;
+const ErrorComponent = ({ error }) => {
+    const err = new Error();
+    // err.message = "인증되지 않은 회원입니다";
+    err.status = {
+        error: {
+            401: {
+                message: "401에러",
+            },
+            404: {
+                message: "404에러",
+            },
+        },
+    };
+    // throw err;
+    // if (err.status === 401) {
+    //     err.message = "인증되지 않은 회원입니다";
+    //     throw err;
+    // } else if (err.status === 404) {
+    //     err.message = "요청하신 리소스를 찾을 수 없습니다";
+    //     throw err;
+    // }
+    return (
+        <>
+            <div>{err.status.error.message}</div>
+        </>
+    );
 };
 export default ErrorComponent;
